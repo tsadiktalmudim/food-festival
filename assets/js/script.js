127c0613366d18157ae05849a7d1f37320cdc939
Xan <tsadiktalmudim@gmail.com>
2022-06-12 15:17:07 -0500

    OFFLINE, BRUH

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,28 @@
-const img1 = require("../../assets/img/food-table.jpg");
-const img2 = require("../../assets/img/grill.jpg");
+require('bootstrap');
+const img1 = require("../img/food-table.jpg");
+const img2 = require("../img/grill.jpg");
 
-require("bootstrap");
-
-// First image is hard coded in index.html
-const carouselSlides = [
-  {
-    title: "We travel all over the US",
-    subtitle: "Check out our schedule!",
-    img: img1,
-    btnText: "View Schedule",
-    btnUrl: "schedule.html",
-  },
-  {
-    title: "Our food is seriously the bomb!",
-    subtitle: "What are you waiting for?",
-    img: img2,
-    btnText: "Purchase Tickets",
-    btnUrl: "tickets.html",
-  },
-];
-
-carouselSlides.forEach((slide, i) => {
-  $(".carousel-inner").append(`
+$(document).ready(function() {
+  // First image is hard coded in index.html
+  const carouselSlides = [
+    {
+      title: "We travel all over the US",
+      subtitle: "Check out our schedule!",
+      img: img1,
+      btnText: "View Schedule",
+      btnUrl: "schedule.html"
+    },
+    {
+      title: "Our food is seriously the bomb!",
+      subtitle: "What are you waiting for?",
+      img: img2,
+      btnText: "Purchase Tickets",
+      btnUrl: "tickets.html"
+    },
+  ];
+  
+  carouselSlides.forEach((slide, i) => {
+    $('.carousel-inner').append(`
   <div class="carousel-item fullscreen-carousel" style="background-image: url('${slide.img}')">
     <div class="d-flex h-100 align-items-center justify-content-center carousel-caption">
         <div class="container">
@@ -39,5 +39,6 @@ carouselSlides.forEach((slide, i) => {
           </div>
         </div>
     </div>
-  </div>`);
+  </div>`)
+  })
 });
